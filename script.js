ad574753459a205ec5e4f8d39c2dc286082dca2a
IANULA CONDREA <andreww.steel69@gmail.com>
2025-12-12 12:31:29 +0200

    add func for ac dot and make the operate function

--- script.js
+++ script.js
@@ -8,6 +8,24 @@ const display = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operation");
 
+const equalBtn = document.querySelector("#equal");
+const dotbtn = document.querySelector("#dot");
+const clearBtn = document.querySelector("#ac");
+
+
+//Function to append the numbers in display
+function handleNumber(number) {
+    if(currentNum.length <= 10) {       //Set a limit to max 10 chars to prevent overflow
+        currentNum += number;
+        
+        if(operator !== "") {
+            display.textContent = `${previousNum} ${operator} ${currentNum}`;
+        } else {
+            display.textContent = currentNum;
+        }
+    }
+}
+
 //Add event listener for all number btns
 
 numberButtons.forEach((btn) => {
@@ -15,13 +33,100 @@ numberButtons.forEach((btn) => {
 })
 
 
-//Function to append the numbers in display
-function handleNumber(number) {
-    if(currentNum.length <= 10) {       //Set a limit to max 10 chars to prevent overflow
-        currentNum += number;
-        display.textContent = currentNum;
+//Function to handle the operator
+function handleOperator(op) {
+
+    if(currentNum === "") {     //Prevent the operator functionaliy if there is not a current number
+        return;
     }
+
+    operator = op;
+    previousNum = currentNum;
+    currentNum = "";
+
+    display.textContent = `${previousNum} ${operator}`;
+
 }
 
+operators.forEach((btn) => {
+    btn.addEventListener("click", (e) => handleOperator(e.target.textContent));
+})
 
-//Function to handle the operator
+//Function to operate the math
+
+function operate() {
+    let result;
+    const num1 = parseFloat(previousNum);
+    const num2 = parseFloat(currentNum);
+
+    //Check if num1 and num2 are actual numbers
+    if(isNaN(num1) || isNaN(num2)) {
+        return;
+    }
+
+    switch(operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "X":
+            result = num1 * num2;
+            break;
+        case "÷":
+            if(num2 === 0) {
+                result = "Cannot divide by 0!";
+                display.textContent = result;
+                currentNum = "";
+                previousNum = "";
+                operator = "";
+                return
+            } 
+            
+            result = num1 / num2;
+            break;
+
+        default: 
+            return;
+    }
+
+    result = Math.round(result * 1000) / 1000;
+
+    currentNum = result.toString();
+    operator = "";
+    previousNum = "";
+
+    display.textContent = result;
+}
+
+equalBtn.addEventListener("click", () => {
+    if(currentNum != "" && previousNum != "") {
+        operate();
+    }
+})
+
+dotbtn.addEventListener("click", () => {
+    if(!currentNum.includes(".")){
+        
+        if(currentNum === "") {
+            currentNum = "0";
+        }
+
+        currentNum += ".";
+
+        if(previousNum !== "" && currentNum !== "") {
+            display.textContent = `${previousNum} ${operator} ${currentNum}`;
+        } else {
+            display.textContent = currentNum;
+        }
+    }
+})
+
+clearBtn.addEventListener("click", () => {
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+
+    display.textContent = "0";
+})
